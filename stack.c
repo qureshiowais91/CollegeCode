82d85b71593ee31c3d964e462f8bb164b848fab4
truckflipper <qureshiowais91@gmail.com>
2020-08-13 16:47:13 +0530

    removed repeated code

--- stack.c
+++ stack.c
@@ -7,6 +7,8 @@
 #define empty -1
 #define size 10
 
+int infixtoPostfix(char *expression);
+
 int pop(char stack[], int *top)
 {
     char operator;
@@ -71,47 +73,52 @@ int display(char output[], int index)
 int string_reverse(char *str)
 {
     int i=0;
-    int j = strlen(str);
+    int j = strlen(str)-1;
 
     for (i = 0; i != j / 2; i++)
     {
-        if (str[j - i] == '(')
-        {
-            str[j - i] = ')';
-            continue;
-        }
-
-        else if (str[j - i] == ')')
-        {
-            str[j - i] = '(';
-            continue;
-        }
-
-        if (str[i] == '(')
-        {
-            str[i] = ')';
-            continue;
-        }
-
-        else if (str[i] == ')')
-        {
-            str[i] = '(';
-            continue;
-        }
-
         str[i] = str[i] + str[j - i];
         str[j - i] = str[i] - str[j - i];
         str[i] = str[i] - str[j - i];
     }
-
+    for ( i = 0;str[i]!='\0'; i++)
+    {
+        if(str[i]=='(')
+        {
+            str[i]=')';
+        }
+        else if(str[i]==')')
+        {
+            str[i]='(';
+        }
+    }
+    
     return 0;
 }
 
-int infixtoPostfix(char expression[])
+int infixtoprefix(char *expression)
+{
+    unsigned short int i;
+    string_reverse(expression);
+    infixtoPostfix(expression);
+    string_reverse(expression);
+     
+     for ( i = 0; expression[i] != '\0' ; i++)
+     { 
+        printf("%c",expression[i]);
+     }
+     
+
+   return 0;
+}
+
+int infixtoPostfix(char *expression)
 {
 
     char output[size];
     char stack[size];
+
+    unsigned short int counter;
 
     unsigned short int stack_priority_value;
     unsigned short int expression_priority_value;
@@ -163,79 +170,10 @@ int infixtoPostfix(char expression[])
         output[index] = pop(stack, &top);
         index++;
     }
-
-    display(output, index);
-    return 0;
-}
-
-int infixtoprefix(char expression[])
-{
-
-    char output[size];
-    char stack[size];
-
-    unsigned short int stack_priority_value;
-    unsigned short int expression_priority_value;
-
-    int top = -1;
-
-    int loop = 0;
-    int index = 0;
-
-    /*1. Scan the infix expression from left to right.*/
-    for (loop = 0; expression[loop] != '\0'; loop++)
+  
+    for (counter=0; index != counter; counter++)
     {
-        /*scanned character is an operand*/
-        if (isalnum(expression[loop]))
-        {
-            output[index] = expression[loop];
-            index++;
-            continue;
-        }
-
-        stack_priority_value = priority(stack[top]);
-        expression_priority_value = priority(expression[loop]);
-
-        if (top == empty || stack_priority_value < expression_priority_value || stack[top] == '(')
-        {
-            push(stack, &top, expression[loop]);
-        }
-        /*pop all the operators from the stack which are greater than or equal priority*/
-        else if (expression[loop] == ')')
-        {
-            while (stack[top] != '(')
-            {
-                output[index++] = pop(stack, &top);
-            }
-            pop(stack, &top);
-        }
-        else if (stack_priority_value >= expression_priority_value)
-        {
-            while (priority(stack[top]) >= expression_priority_value && top != empty)
-            { /*if top == -1 pop return 0 !*/
-                output[index++] = pop(stack, &top);
-            }
-            push(stack, &top, expression[loop]);
-        }
-    }
-
-    while (top != empty)
-    {
-        output[index] = pop(stack, &top);
-        index++;
-    }
-
-    while ((index + 1))
-    {
-        /*
-        if(output[index]==')' || output[index]=='(')
-        {
-            index--;
-            continue;
-        }
-        */
-        printf("%c", output[index]);
-        index--;
+        expression[counter]=output[counter];
     }
 
     return 0;
@@ -248,8 +186,7 @@ int main()
 
     printf("Enter Expression :");
     scanf("%s", expression);
-
-    string_reverse(expression);
+    
     infixtoprefix(expression);
 
     return 0;
